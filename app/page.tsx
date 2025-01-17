cf95cad574bafcfdec0ffd2fe535b17586339283
Francois Hill RapidDeploy <francois.hill@rappiddeploy.com>
2025-01-17 13:32:05 +0200

    Add accordion component to homepage for better content organization

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@mantine/core';
+import { Accordion, AccordionControl, AccordionItem, AccordionPanel, Text } from '@mantine/core';
 import { getFileContent } from '../api/getFileContent';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
@@ -12,6 +12,14 @@ export default async function HomePage() {
       <ColorSchemeToggle />
 
       <Text c="green">fileContent: {fileContent}</Text>
+
+      <Accordion variant="separated">
+        <AccordionItem value="First item">
+          <AccordionControl>First item content</AccordionControl>
+
+          <AccordionPanel>First item content</AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </>
   );
 }
